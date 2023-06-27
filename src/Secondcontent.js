4baa20a27af45ca5f50c277218959bf55fb42362
Jasper Bergantinos <jasperbergantinos3@gmail.com>
2023-06-27 17:49:48 +0800

    updated responsiveness june 27

--- src/Secondcontent.js
+++ src/Secondcontent.js
@@ -27,7 +27,7 @@ function Secondcontent() {
 		],
 	};
 	return (
-		<div className="bg-black lg:top-40 md:top-18 w-full m-auto h-full z-50 relative mt-72 grid grid-cols-1  justify-items-center ">
+		<div className="bg-black lg:top-40 md:top-18 w-full m-auto h-full z-30 relative mt-72 grid grid-cols-1  justify-items-center ">
 			<div className=" ml-8 mt-24 h-3/4 grid grid-cols-1 text-5xl text-red-700  tracking-wide font-bold font-unifrakturCook z-50 ">
 				{" "}
 				Meet the Choreographer
